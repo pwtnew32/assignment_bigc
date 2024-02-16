70fec819da8a51306d91c06ad5c44e2cd2099d13
Phawat Mekhaphan <phawatm@Phawats-MacBook-Pro.local>
2024-02-16 13:56:28 +0700

    add create table script

---
+++ sql/create_table_products.sql
@@ -0,0 +1,9 @@
+create table warehouse.products(
+category varchar(100),
+product varchar(500),
+brand varchar(255),
+price_per_unit float,
+unit varchar(100),
+store varchar(255),
+time_stamp timestamp
+)
